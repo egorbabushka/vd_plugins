335f17df91c0b268abc9c2f6dc7bcc6964bb8b1d
egorbabushka <130289288+egorbabushka@users.noreply.github.com>
2023-05-21 01:16:19 +0300

    Add files via upload

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,54 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { registerCommand } from "@vendetta/commands"
+import { toasts } = from "@vendetta/metro/common"
+
+
+const ask = async (args, ctx) => {
+  const url = 'https://gptcustomapi.ieghorbabushka1.repl.co/v1/completion';
+  const body = JSON.stringify({
+    'content': args[0]
+  });
+  console.log(body)
+  const headers = {
+    'Content-Type': 'text/plain'
+  }
+  
+  fetch(url, {
+    method: 'POST',
+    headers,
+    body,
+  })
+    .then(response => response.json())
+    .then(data => {
+      return {content: data.choices[0].text}
+    });
+
+}
 
 export default {
     onLoad: () => {
-        logger.log("Hello world!");
+        toasts.open({content: "hello, tehello
+        const u = registerCommand({
+            name: "Ask_ChatGPT", 
+            displayName: "Ask ChatGPT",
+            displayDescription: "ask chatgpt",
+            description: "",
+            options: [{
+                name: "prompt",
+                description: "prompt",
+                type: 3,
+                required: !0,
+                displayName: "Prompt",
+                displayDescription: ""
+            }],
+            execute: ask,
+            applicationId: "-1",
+            inputType: 1,
+            type: 1
+        })
     },
     onUnload: () => {
-        logger.log("Goodbye, world.");
+        toasts.open({content: "goodbye, test"})
+        u()
     },
     settings: Settings,
 }
