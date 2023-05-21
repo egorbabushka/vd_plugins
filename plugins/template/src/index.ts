18930fda5848717ecff69e9aa92c5222e26f6dfc
egorbabushka <130289288+egorbabushka@users.noreply.github.com>
2023-05-21 14:42:49 +0300

    Add files via upload

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -13,7 +13,7 @@ const MessageUtils = findByProps(
 const ask = async (args, ctx) => {
   var optionstest = `egorbabushka: ${args[0]}, ${args[1]}`
   logger.log(optionstest)
-  var isSend = args[1].value ?? false
+  const isSend = args[1].value ?? false
   const url = 'https://gptcustomapi.ieghorbabushka1.repl.co/v1/completion';
   const body = JSON.stringify({
     'content': args[0].value
@@ -30,6 +30,7 @@ const ask = async (args, ctx) => {
   })
     .then(response => response.json())
     .then(data => {
+      logger.log(`egorbabushka: ${isSend}`)
       if (!isSend) {
         ClydeUtils.sendBotMessage(ctx.channel.id, data.choices[0].text)
       } else {
