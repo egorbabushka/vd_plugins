4537555c7b7cebe630445fdbc9502bb1470aef20
egorbabushka <130289288+egorbabushka@users.noreply.github.com>
2023-05-21 13:30:26 +0300

    Add files via upload

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -10,6 +10,7 @@ const ClydeUtils = findByProps("sendBotMessage")
 
 const ask = async (args, ctx) => {
   var optionstest = `egorbabushka: ${args[0]}, ${args[1]}`
+  logger.log(optionstest)
   const url = 'https://gptcustomapi.ieghorbabushka1.repl.co/v1/completion';
   const body = JSON.stringify({
     'content': args[0].value
